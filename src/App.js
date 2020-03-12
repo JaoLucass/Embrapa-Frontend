32e86fc2c37f72814193124c9510d0ca0212ad9b
João Lucas <JoaoLucaSidney@outlook.com>
2020-03-11 21:18:11 -0400

    comunicação com o backend implementada

--- src/App.js
+++ src/App.js
@@ -1,64 +1,105 @@
 import './App.css';
-import React, * as react from 'react';
+import React, {Component, useState, useEffect} from 'react';
 import {LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend} from 'recharts';
+import axios from "axios";
 
-const data = [
-  {name: 'Page A', pv: 2400, amt: 2400},
-  {name: 'Page B', pv: 1398, amt: 2210},
-  {name: 'Page C', pv: 9800, amt: 2290},
-  {name: 'Page D', pv: 3908, amt: 2000},
-  {name: 'Page E', pv: 4800, amt: 2181},
-  {name: 'Page F', pv: 3800, amt: 2500},
-  {name: 'Page G', pv: 4300, amt: 2100},
-];
-
-class SimpleLineChart extends react.Component {
-  
-  render () {
-    return (
-      <LineChart
-        width={600}
-        height={300}
-        data={data}
-        margin={{top: 5, right: 30, left: 20, bottom: 5}}
-        >
-        <Line
-          type='monotone'
-          dataKey='pv'
-          stroke='#8884d8'
-          activeDot={{r: 8}}
-          />
-        <CartesianGrid strokeDasharray='3 3'/>
-        <Tooltip/>
-        <YAxis/>
-        <XAxis dataKey='name'/>
-        <Legend />
-      </LineChart>
-    );
-  }
+class App extends Component{
+	
+	constructor(props){
+		super(props);
+		this.state = {
+			items: [],
+			isLoaded: false,
+		}
+	}
+	
+	componentDidMount(){
+		fetch('https://luis-back-node.herokuapp.com/dados_da_estacao')
+			.then(res => res.json())
+			.then(json => {
+				this.setState({
+					isLoaded: true,
+					items: json,
+				})
+			});
+	}
+		
+	render(){
+		
+		var { isLoaded, items } = this.state;
+		
+		
+		if(!isLoaded){
+			return <div>Loading...</div>;
+		}
+		
+		else{
+						
+			console.log(items);
+			var strTemp = '';
+			var vector = [];
+			var L = 0;
+			var C = 0;
+			while (L < 4){
+				while (C < 19) {
+					strTemp += items[L].createdAt[C];
+					C+=1;
+				}
+				vector[L]=strTemp;
+				strTemp = '';
+				C=0;
+				L++;
+			}
+	
+			
+			const data = [
+				{data: vector[0], precipitacao: 0},
+				{data: vector[1], precipitacao: 20},
+				{data: vector[2], precipitacao: items[2].precipitacao},
+				{data: vector[3], precipitacao: items[3].precipitacao},
+				{data: '2020-02-13T16:32:23', precipitacao: 100},
+				{data: '2020-02-13T17:35:51', precipitacao: 40},
+				{data: '2020-02-13T18:23:09', precipitacao: 70},
+			];
+			
+				return (
+					<div className="App">
+					
+						<select>
+							<option value="daily">Dia</option>
+							<option value="weekly">Semana</option>
+							<option value="Month">Mês</option>
+							<option value="Three_months">3 Meses</option>
+							<option value="six_months">6 Meses</option>
+							<option value="1_year">1 ano</option>
+						</select>				
+				
+						<LineChart
+							width={1300}
+							height={600}
+							data={data}
+							margin={{top: 5, right: 30, left: 20, bottom: 5}}
+							>
+							<Line
+								type='monotone'
+								dataKey='precipitacao'
+								stroke='#8884d8'
+								activeDot={{r: 8}}
+								/>
+							<CartesianGrid strokeDasharray='3 3'/>
+							<Tooltip/>
+							<YAxis/>
+							<XAxis dataKey='data'/>
+							<Legend />
+						</LineChart>
+						
+					</div>
+				);
+			
+		
+		}
+	}
 }
 
-export default SimpleLineChart;
 
-/*function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;*/
+export default App;
